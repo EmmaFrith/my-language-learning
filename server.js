eaf0da3dbc06897ed10b6abc0fc1a4f19607ad14
Emma Frith <emma.frith@education.gov.uk>
2024-05-27 22:43:11 +0100

    post routes functioning to display mongo db data

--- server.js
+++ server.js
@@ -12,16 +12,17 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-  });
+});
 
 
 const methodOverride = require('method-override');
 
 const morgan = require('morgan');
 
-const Word = require('./models/words.js');
+const Words = require('./models/words.js');
 
-const Phrase = require('./models/phrases.js');
+const Phrases = require('./models/phrases.js');
+
 
 app.use(express.json())
 
@@ -36,28 +37,44 @@ const port = process.env.PORT ? process.env.PORT : 3000;
 
 app.listen(port, () => {
     console.log("Listening on port", process.env.PORT);
-  });
+});
 
 
-  app.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.render("home.ejs");
-  });
+});
 
-  app.get("/my-words", (req, res) => {
-    res.render("words/my-words.ejs");
-  });
+ app.get("/my-words", async (req, res) => {
+    const words = await Words.find()
+     res.render("words/my-words.ejs", {
+        words
+     });
+ });
 
-  app.get("/my-phrases", (req, res) => {
-    res.render("phrases/my-phrases.ejs");
-  });
+app.get("/my-phrases", async (req, res) => {
+    const phrases = await Phrases.find()
+    res.render("phrases/my-phrases.ejs", {
+        phrases
+    });
+});
 
-  app.get("/my-words/new-word", (req, res) => {
+app.get("/my-words/new-word", (req, res) => {
     res.render("words/new-word.ejs");
-  });
+});
 
-  app.get("/my-phrases/new-phrase", (req, res) => {
+app.get("/my-phrases/new-phrase", (req, res) => {
     res.render("phrases/new-phrase.ejs");
-  });
+});
 
+app.post("/my-words", async (req, res) => {
+    console.log(req.body)
+    await Words.create(req.body)
+    res.redirect('/my-words')
+})
 
+app.post("/my-phrases", async (req, res) => {
+    console.log(req.body)
+    await Phrases.create(req.body)
+    res.redirect('/my-phrases')
+})
 
